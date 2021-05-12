acedc839a387f1e77f63698e7613adb9b23d3071
timurcabr <timurcabr@gmail.com>
2021-05-12 16:48:30 +0600

    CRUD with context menu is ready

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,36 +1,47 @@
 package com.example.notesapp
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.content.Context
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.notesapp.fragments.AboutFragment
-import com.example.notesapp.fragments.AddNoteFragment
 import com.example.notesapp.fragments.NotesFragment
+import com.example.notesapp.models.Note
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.textfield.TextInputLayout
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
+    val gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        1. Создайте список ваших заметок.
-//        2. Создайте карточку для элемента списка.
-//        3. Класс данных, созданный на шестом уроке, используйте для заполнения карточки списка.
-//        4. * Создайте фрагмент для редактирования данных в конкретной карточке. Этот фрагмент пока можно вызвать через основное меню.
+//        + 1. Сделайте фрагмент добавления и редактирования данных, если вы ещё не сделали его.
+//        2. Сделайте навигацию между фрагментами, также организуйте обмен данными между фрагментами.
+//        3. Создайте контекстное меню для изменения и удаления заметок.
+//        4. * Изучите, каким образом можно вызывать DatePicker в виде диалогового окна. Создайте текстовое поле, при нажатии на которое вызывалось бы диалоговое окно с DatePicker.
 
         drawerLayout = findViewById(R.id.drawerLayout)
         navigationView = findViewById(R.id.navigationMenu)
@@ -50,10 +61,85 @@ class MainActivity : AppCompatActivity() {
         fragmentOrientation()
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
-            AddNoteFragment().show(supportFragmentManager, "Add Note Fragment")
+            openAddDialog()
         }
     }
 
+    @SuppressLint("InflateParams")
+    private fun openAddDialog() {
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Add Note")
+            .setMessage("Please fill in the blanks to create a new note!")
+            .setIcon(R.drawable.ic_baseline_note_add_24)
+
+        val view = LayoutInflater.from(this).inflate(R.layout.dialog_add_note, null)
+        dialog.setView(view)
+
+        val noteTitle = view.findViewById<TextInputLayout>(R.id.noteTitle_add)
+        val noteDesc = view.findViewById<TextInputLayout>(R.id.noteDesc_add)
+        val selectedDateView = view.findViewById<TextView>(R.id.selectedDate)
+        var selectedDate = ""
+
+        view.findViewById<Button>(R.id.selectDate_add).setOnClickListener {
+            val myCalendar = Calendar.getInstance()
+            val year = myCalendar.get(Calendar.YEAR)
+            val month = myCalendar.get(Calendar.MONTH)
+            val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+            this.let {
+                DatePickerDialog(
+                    it,
+                    { _, selectedYear, selectedMonth, selectedDay ->
+                        selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                        selectedDateView.text = (selectedDate)
+                    },
+                    year,
+                    month,
+                    day
+                ).show()
+            }
+        }
+        dialog.setPositiveButton("Add Note") { _, _ ->
+            if (noteTitle.editText?.text.toString().isNotEmpty()
+                && noteDesc.editText?.text.toString().isNotEmpty()
+                && selectedDate != ""
+            ) {
+                val note = Note(
+                    noteTitle.editText?.text.toString(),
+                    noteDesc.editText?.text.toString(),
+                    convertDate(selectedDate)
+                )
+                val sharedPreferences =
+                    getSharedPreferences(Utils.SHARED_DB_NAME, Context.MODE_PRIVATE)
+                if (sharedPreferences?.getString(Utils.DATA_LIST, null) != null) {
+                    val listType = object : TypeToken<MutableList<Note>>() {}.type
+                    val json = sharedPreferences.getString(Utils.DATA_LIST, null)
+                    val userNotes: MutableList<Note> = gson.fromJson(json, listType)
+                    userNotes.add(note)
+                    setSharedPreferences(userNotes)
+                }
+            }
+            setFragment(NotesFragment())
+        }
+
+        dialog.setNegativeButton("Cancel") { _, _ ->
+
+        }
+        dialog.show()
+    }
+
+    private fun convertDate(pickedDate: String): Date {
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+        return sdf.parse(pickedDate) as Date
+    }
+
+    private fun setSharedPreferences(userNotes: MutableList<Note>) {
+        val sharedPreference = getSharedPreferences(Utils.SHARED_DB_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreference?.edit()
+        val userNotesString = gson.toJson(userNotes)
+        editor?.putString(Utils.DATA_LIST, userNotesString)
+        editor?.apply()
+    }
 
     private fun setFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
@@ -94,3 +180,4 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 }
+
--- app/src/main/java/com/example/notesapp/fragments/NotesFragment.kt
+++ app/src/main/java/com/example/notesapp/fragments/NotesFragment.kt
@@ -5,23 +5,23 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.notesapp.models.Note
-import com.example.notesapp.adapters.NotesAdapter
 import com.example.notesapp.R
 import com.example.notesapp.Utils
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.notesapp.adapters.NotesAdapter
+import com.example.notesapp.models.Note
 import com.google.gson.Gson
 import java.util.*
 
 class NotesFragment : Fragment(), NotesAdapter.OnItemClickListener {
 
-    private val userNotes = mutableListOf<Note>()
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: NotesAdapter
+    private var userNotes = mutableListOf<Note>()
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: NotesAdapter
     private val gson = Gson()
 
     override fun onCreateView(
@@ -35,23 +35,15 @@ class NotesFragment : Fragment(), NotesAdapter.OnItemClickListener {
         super.onViewCreated(view, savedInstanceState)
 
         userNotes.clear()
+        updateList()
 
-        val preferences = context?.getSharedPreferences(Utils.SHARED_DB_NAME, Context.MODE_PRIVATE)
-        if (preferences?.getString(Utils.DATA_LIST, null) != null) {
-            userNotes.addAll(
-                gson.fromJson(
-                    preferences.getString(Utils.DATA_LIST, null),
-                    Array<Note>::class.java
-                )
-            )
-            Log.i("MyData", userNotes.size.toString())
-        }
+        checkListSize(view)
 
         recyclerView = view.findViewById(R.id.notesList)
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.setHasFixedSize(true)
 
-        adapter = NotesAdapter(userNotes, this)
+        adapter = context?.let { NotesAdapter(it, userNotes, this) }!!
         recyclerView.adapter = adapter
         registerForContextMenu(recyclerView)
         setSharedPreferences(userNotes)
@@ -76,6 +68,7 @@ class NotesFragment : Fragment(), NotesAdapter.OnItemClickListener {
                 .addToBackStack(null)
                 .commit()
         }
+        view?.let { checkListSize(it) }
     }
 
     override fun onDeleteClick(position: Int) {
@@ -83,6 +76,7 @@ class NotesFragment : Fragment(), NotesAdapter.OnItemClickListener {
         userNotes.remove(userNotes[position])
         setSharedPreferences(userNotes)
         adapter.notifyDataSetChanged()
+        view?.let { checkListSize(it) }
     }
 
     override fun onEditClick(position: Int) {
@@ -98,6 +92,19 @@ class NotesFragment : Fragment(), NotesAdapter.OnItemClickListener {
             .commit()
     }
 
+    fun updateList(){
+        val preferences = context?.getSharedPreferences(Utils.SHARED_DB_NAME, Context.MODE_PRIVATE)
+        if (preferences?.getString(Utils.DATA_LIST, null) != null) {
+            userNotes.addAll(
+                gson.fromJson(
+                    preferences.getString(Utils.DATA_LIST, null),
+                    Array<Note>::class.java
+                )
+            )
+            Log.i("MyData", userNotes.size.toString())
+        }
+    }
+
     private fun setSharedPreferences(userNotes: MutableList<Note>) {
         val sharedPreference =
             context?.getSharedPreferences(Utils.SHARED_DB_NAME, Context.MODE_PRIVATE)
@@ -105,6 +112,14 @@ class NotesFragment : Fragment(), NotesAdapter.OnItemClickListener {
         val userNotesString = gson.toJson(userNotes)
         editor?.putString(Utils.DATA_LIST, userNotesString)
         editor?.apply()
+    }
+
+    private fun checkListSize(view: View) {
+        if (userNotes.size == 0) {
+            view.findViewById<TextView>(R.id.message).visibility = View.VISIBLE
+        } else {
+            view.findViewById<TextView>(R.id.message).visibility = View.GONE
+        }
     }
 
     override fun onDestroy() {
